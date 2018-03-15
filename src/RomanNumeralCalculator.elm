378db2aebf3b8fa572c08654610efee933af9a9f
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-14 23:50:16 -0400

    Updated to have working test using pattern matching

--- src/RomanNumeralCalculator.elm
+++ src/RomanNumeralCalculator.elm
@@ -1,4 +1,20 @@
 module RomanNumeralCalculator exposing (..)
 
 addRomanNumerals : String -> String -> String
-addRomanNumerals first second = "II"
+addRomanNumerals first second =
+    (toArabic first) + (toArabic second)
+    |> toRoman
+
+toArabic: String -> Int
+toArabic roman =
+    case roman of
+        "I" -> 1
+        "II" -> 2
+        _ -> 0
+
+toRoman: Int -> String
+toRoman arabic =
+    case arabic of
+        2 -> "II"
+        3 -> "III"
+        _ -> ""
